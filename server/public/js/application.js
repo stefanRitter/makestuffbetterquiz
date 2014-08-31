62a288b5ac55927f4d71eb2467afd06e3c388c2e
stefanRitter <stefan@stefanritter.com>
2014-08-31 08:53:33 +0100

    remove header

--- server/public/js/application.js
+++ server/public/js/application.js
@@ -360,23 +360,6 @@ angular.module('app').run(function ($rootScope, $location) {
   });
 });
 
-angular.module('app').controller('headerController', ['$location', '$window', function ($location, $window) {
-  'use strict';
-
-  var vm = this;
-  vm.hideOn = function () {
-    for (var i = 0, len = arguments.length; i < len; i++) {
-      if (arguments[i] === $location.path()) {
-        return true;
-      }
-    }
-  };
-
-  vm.back = function () {
-    $window.history.back();
-  };
-}]);
-
 angular.module('app').factory('Question', ['$resource', function ($resource) {
   'use strict';
 
